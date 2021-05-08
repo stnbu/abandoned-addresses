aa7d2c1ba27d1c6df527e344de74bae554179680
Mike Burr <mburr@unintuitive.org>
2021-05-08 15:56:46 -0400

    v1

---
+++ contracts/Blacklist.sol
@@ -0,0 +1,22 @@
+// The idea has the advantage of being super simple! Create a contract that cointains blacklisted addresses.
+
+// UNTESTED UNCOMPILED pseodocode ...
+
+pragma solidity ^0.8.0;
+
+contract Blacklist {
+
+    private mapping(address => bool) blacklist;
+
+    // We have no owner, nor anything to construct.
+    // We get by on our own reputation alone.
+
+    public function isBlacklisted(address _address) (bool) {
+	return blacklist[_address];
+    }
+
+    public function blacklistAddress(address _address) {
+	require(msg.sender == _address);
+	blacklist[msg.sender] = true;
+    }
+}
