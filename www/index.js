53d03518662f02d7ff44b51e351468a537f3ecb1
Mike Burr <mburr@unintuitive.org>
2021-05-11 15:39:14 -0400

    working "connect"

--- www/index.js
+++ www/index.js
@@ -1,5 +1,12 @@
+const ethereumButton = document.querySelector('#enableEthereumButton');
 
-const ethereumButton = document.querySelector("#enableEthereumButton");
-ethereumButton.addEventListener("click", () => {
-  ethereum.enable();
+ethereumButton.addEventListener('click', () => {
+  getAccount();
 });
+
+async function getAccount() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+}
+
+let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
