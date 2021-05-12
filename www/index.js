0a5262ff464488c2ad34eadca373cc847c99e2a0
Mike Burr <mburr@unintuitive.org>
2021-05-12 15:49:10 -0400

    closes #1

--- www/index.js
+++ www/index.js
@@ -1,8 +1,6 @@
 
-// const ethereumButton = document.querySelector('#enableEthereumButton');
-
 var account;
-var web3 = new Web3(Web3.givenProvider || "ws://localhost:8545"); // do we really care to do the `||` thing? not worth putting in the code imho.
+var web3 = new Web3(Web3.givenProvider);
 web3.eth.defaultAccount = web3.eth.accounts[0];
 var contractAddress = "0x5e2B9ba689fBB01ADB928044a31e331a8a1C31D4"; // obviously we need a better-than-this
 // and here. what's the smart way to streamline all this stuff?
@@ -48,38 +46,45 @@ var abi = [
 ];
 
 var Blacklist = new web3.eth.Contract(abi, contractAddress);
-debugger;
+
 async function getAccount() {
-  account = await ethereum.request({ method: 'eth_requestAccounts' })[0];
+  ethereum.request({ method: 'eth_requestAccounts' }).then((accounts) => {
+    account = accounts[0];
+  });
 }
 
-$("#blacklistAddress").click(function() {
+$("#enableEthereumButton").click(function() {
   // do noop if account already set. or do we need to?
   getAccount();
 });
 
-Blacklist.methods.isBlacklisted(function(error, result) {
-  if (!error) {
-    // update html element
-    console.log(result);
-  } else
-    console.log(error);
-});
+//
+// TBD
+//
+// Blacklist.methods.isBlacklisted(function(error, result) {
+//   if (!error) {
+//     console.log(result);
+//   } else
+//     console.log(error);
+// });
 
 $("#blacklistAddress").click(function() {
   let address = $("#blacklistedAddress").val();
-  console.log("entering");
-  Blacklist.methods.blacklistAddress(address).send({from: account})
+  transaction = Blacklist.methods.blacklistAddress(address);
+  transaction.send({from: account})
     .on('transactionHash', function(hash){
-      console.log("hash");
+      console.log("hash: " + hash);
     })
     .on('confirmation', function(confirmationNumber, receipt){
-      console.log("conf");
+      console.log("hash: " + hash);
     })
     .on('receipt', function(receipt){
-      console.log("recp");
+      console.log("receipt: " + receipt);
     })
     .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-      console.log("err");
+      console.log("error/receipt: " + error + "/" + receipt);
+    })
+    .finally(function() {
+      console.log("finally!");
     });
 });
