924b89ce8d1ff7e25182b66970a20c045d6a59d7
Mike Burr <mburr@unintuitive.org>
2021-05-14 14:28:00 -0400

    much comment neatening. re-indent (2)

--- www/index.js
+++ www/index.js
@@ -1,82 +1,127 @@
+// SPDX-License-Identifier: UNLICENSED
 /*
-Outstanding weird bug...
+  Interact with the reference Blacklist contract on Rinkeby
 
-When I switch wallets in MM, and then do a "blacklistAddress", I get a weird error deep in the guts that I cannot diagnose.
-If I do a subsequent blacklistAddress with the now-switched-to wallet, is ok.
-UPDATE: Maybe not... need to examine [object] passed to listeners
+  URL: https://dapps.unintuitive.org/blacklist/
 
-Stuff to be investigated/implemented...
+  1. Switch to "Rinkeby" in MetaMask, and click on [Enable Ethereum].
+  1. Open browser's javascript console.
+  1. Interact and watch the logs.
 
-  Web3.givenProvider  // <-- what is this, do we care?
-  web3.eth.accounts.getAccounts()  // <-- do we care?
-  //// To sign a transaction
-  web3.eth.accounts.signTransaction({
-      to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
-      value: '1000000000',
-      gas: 2000000
-  }, '0x4c0883a69102937d6231471b5dbb6204fe5129617082792ae468d01a3f362318')
-  .then(console.log);
-  //// To resolve a *.eth name
-  web3.eth.ens.resolver('ethereum.eth').then(function (contract) {
-      console.log(contract);
-  });
+  Contributors.
 
+  mb@unintuitive.org
+  adam_AT_THE_D0MA1N_CALLED_americanretailusa.com 
+
+  [Ugly notes moved to the bottom]
 */
 
-// FIXME, ALERT: I thought this always agreed with MM's "connected" indicator. I've seen once where MM was "connected" but this was null !
+var CONTRACT_ADDRESS = "0x5e2B9ba689fBB01ADB928044a31e331a8a1C31D4";
+
 console.log("selectedAddress we are using upon loading: " + window.ethereum.selectedAddress);
-// if window.ethereum.selectedAddress has a non-null value at this point, we could disable the [enable ethereum] button here -- $("#enableEthereumButton")
+// if window.ethereum.selectedAddress has a non-null value at this point, we could disable the [Enable Ethereum]
+// button here -- $("#enableEthereumButton")
+// FIXME: I thought this always agreed with MM's "connected" indicator. I've seen once where MM was "connected" but this
+// was null !
+// UPDATE: if metamask extension disabled, the above throws an "undefined" error, as expected.
 
 if (typeof window.ethereum === 'undefined') {
   alert('Wallet provider is NOT Available!!');  // How do we respond?
 }
 
+function logEvent(eventName, content, _error) {
+  let message = "[event fired: '" + eventName + "'] " + "content=" + JSON.stringify(content);
+  if (_error) {
+    message = message + ", error=" + JSON.stringify(_error);
+  }
+  console.log(message);
+}
+
 var web3 = new Web3(window.ethereum);  // FYI, Web3.givenProvider appears to be an alias for window.ethereum
 
-// passes address="0xfffff...." (string, address)
-window.ethereum.on('connect', function(address) {
+// passes passes connectionInfo which may or may not have a chainId memeber.
+window.ethereum.on('connect', function(connectInfo) {
+  logEvent('connect', connectInfo);
+  //
+  // This event appears to be about "connected to network foobar for the first time" and that's it. When you load the page
+  // for the first time, if MM is enabled, even if you are "not connected", this will fire with the chain that MM is pointing
+  // to.
+  //
+  // WEIRD! -- if we are not "connected" in the MM sense, this event still fires. "we are connected to network foo", even
+  // though MM says "not connected". This 'connect' seems to be ONLY about chains connecting for the first time. Not when
+  // chains switch. If I "connect" in the wallet sense, this does not fire!
   // this fires when you eth_requestAccounts ... NOT when the pages gets loaded and you're already connected
-  console.log("connected with " + address);
+  // Update, ok, now I see {"chainId":"0x1"} ... I guess the passed arg is context-dependant. Watch this space.
+  // You might expect switching to a network would fire this. Nope. Look to 'chainChanged' for that.
+  //
+  if (typeof connectInfo.chainId && connectInfo.chainId === "0x4") {
+    // code that responds to "we are now on rinkeby"
+  } else if (typeof connectInfo.chainId && connectInfo.chainId !== "0x4") {
+    // code that responds to "we are now on some chain but it's not rinkeby"
+  } else {
+    debugger;
+    // this message did not have a `chainId` (is this an error?)
+  }
 });
 
 // passes ..nothing? empty string?
-window.ethereum.on('disconnect', function(x) {
-  console.log("AN address disconnected. If there's an `x` it's: " + x);
+window.ethereum.on('disconnect', function(content) {
+  // I do not recall seeing this fire. Update here.
+  logEvent('disconnect', content);
 });
 
 // passes account="0xffff...." (string, address)
-window.ethereum.on('accountsChanged', function(account) { // my guess, this fires when window.ethereum.selectedAddress changes.
+window.ethereum.on('accountsChanged', function(account) {
+  logEvent('accountsChanged', account);
+  // It looks like this fires if you press "Enable Ethereum" and end up having to "Confirm".
+  // If you click it and get no confirm, this will not fire, but
   //
-  // I believe this is the ONLY place we need to keep up with changes to the "default" address, a.k.a. "window.ethereum.selectedAddress"
-  // MetaMask tries to keep up with the wallet the user thinks they're using. It sets window.ethereum.selectedAddress accordingly and this event gets fired.
-  // So for example, this is where we would enable, disable the "connect to ethereum" button and update any badges or displayed addresses.
-  // Empty string means we are not connected at all. Any other value is the address of the global wallet that this dapp thinks its using (!!!)
-  // Sublty: it looks like the "account" returned when not connected is an empty string, but window.ethereum.selectedAddress gets set to `null`
+  //   window.ethereum.request({ method: 'eth_requestAccounts' })
   //
-  // If and only if `account === ""` should we enable $("#enableEthereumButton")
-  // If account has a non-empty value $("#enableEthereumButton") should be enabled here
-  console.log("[WE WILL REACT TO] accounts changed TO `" + account + "`");
+  // will return the address(es) regardless.
+  //
+  // An empty array means we are not connected at all.
+  //
+  // Sublty: it looks like the "accounts" returned when not connected is an empty array, but
+  // window.ethereum.selectedAddress gets set to `null`
+  //
+  // If and only if `accounts === []` should we enable $("#enableEthereumButton")
+  // If account has a non-empty value $("#enableEthereumButton") should be DISABLED here
+  // (...I think)
 });
 
-// passes toChain="0x5" (string, chain ID) [see: https://chainid.network/]
+// passes toChain="0x5" (A STRING!!!!!!)
+// [see: https://chainid.network/]
 window.ethereum.on('chainChanged', function(toChain) {
-  // We COULD have separate testbeds for each testnet . The contract address (or even the existence of the contract) will be chain-dependent.
+  logEvent('chainChanged', toChain);
+  // NOTE: this DOES fire even when the user is not "connected"
+  // We COULD have separate testbeds for each testnet . The contract address (or even the existence of the contract)
+  // will be chain-dependent.
+  //
   // Or... we could simply have a mapping:
   //
   //  blacklistContractAddresses = {"0x5": "0xffffff..."}  // mapping of chain ID and contract address.
   //
-  // Change the contract value, re-init Blacklist and put up a conspicous banner if we are on a testnet (maybe nothing if mainnet)
+  // Change the contract value, re-init BLACKLIST and put up a conspicous banner if we are on a testnet
+  // (maybe nothing if mainnet)
   //
   // Also, using the same info (contract address) we could have a link to the correct etherscan page for the contract.
-  console.log("chain changed TO " + toChain);
+  // Note that when you e.g. load the page 'connect' with chainId is fired. When you witch chains, this is fired. They
+  // are different events.
+  if (toChain === "0x4") {
+    // code that responds to "we switched from chain X to Y, where Y=0x4
+  } else if (toChain !== "0x4") {
+    // code that responds to "we switched from chain X to Y, where Y!=0x4
+  } else {
+    // Handle this! I don't recall seeing anything land here.
+    alert("handle me!");
+  }
 });
 
 // passes ...some message
-window.ethereum.on('message', function(mail) {
+window.ethereum.on('message', function(content) {
+  logEvent('message', content);
   // Anything that shows up here should be explicitly handled
-  // I'm tempted to say that anything that shows up in the blacklistAddress handlers will show up here too, but maybe not.
-  // It would be nice to know if we have to use those handlers or if the messages here have the same info...
-  console.log("You've Got Mail! " + mail);
 });
 
 function getBlacklistContract() {
@@ -122,15 +167,14 @@ function getBlacklistContract() {
     }
   ];
 
-  let contractAddress = "0x5e2B9ba689fBB01ADB928044a31e331a8a1C31D4";
-  return new web3.eth.Contract(abi, contractAddress);
+  return new web3.eth.Contract(abi, CONTRACT_ADDRESS);
 }
 // If we're going to be using globals, they should appear at the top of the file.
-var Blacklist = getBlacklistContract();
+var BLACKLIST = getBlacklistContract();
 
 async function getAccount() {
   window.ethereum.request({ method: 'eth_requestAccounts' }).then((accounts) => {
-    console.log("we succesfully connected to *A* wallet. accounts==" + accounts);
+    console.log("we succesfully connected to *A* wallet. the data we got was `" + JSON.stringify(accounts) + "`");
   });
 }
 
@@ -140,7 +184,7 @@ $("#enableEthereumButton").click(function() {
 
 $("#getIsBlacklisted").click(function() {
   let address = $("#isBlacklisted").val();
-  transaction = Blacklist.methods.isBlacklisted(address);
+  transaction = BLACKLIST.methods.isBlacklisted(address);
   transaction.call({from: window.ethereum.selectedAddress}, function (error, isBlacklisted) {
     if (!error) {
       if (isBlacklisted) {
@@ -156,22 +200,50 @@ $("#getIsBlacklisted").click(function() {
 
 $("#blacklistAddress").click(function() {
   let address = $("#blacklistedAddress").val();
-  transaction = Blacklist.methods.blacklistAddress(address);
+  transaction = BLACKLIST.methods.blacklistAddress(address);
   transaction.send({from: window.ethereum.selectedAddress})
     .on('transactionHash', function(hash){
-      console.log("transaction hash: " + hash);
+      logEvent('transactionHash', hash);
     })
-    .on('confirmation', function(confirmationNumber, receipt){
-      // We have the confirmation number here. If someone blacklists an address, we could have a widget that shows a confirmation count.
-      console.log("blacklisting confirmed!: " + receipt + " (" + confirmationNumber + ")");
+    .on('confirmation', function(confirmationNumber, receipt) {
+      // NOTE: we abuse error= field for confirmation number
+      logEvent('confirmation', receipt, "(conf_num=" + confirmationNumber + "[not_an_error])");
+      // We have the confirmation number here. If someone blacklists an address, we could have a widget
+      // that shows a confirmation count
     })
     .on('receipt', function(receipt){
-      console.log("receipt: " + receipt);
+      logEvent('receipt', reciept);
     })
-    .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-      console.log("error/receipt: " + error + "/" + receipt);
+    .on('error', function(error, receipt) {
+      // If the transaction was rejected by the network with a receipt,
+      // the second parameter will be the receipt.
+      logEvent('error', receipt, error);
     })
     .finally(function() {
-      console.log("blacklisting finally clause");
+      // always runs (for each event?)
     });
 });
+
+/*
+  Outstanding weird bug...
+
+  When I switch wallets in MM, and then do a "blacklistAddress", I get a weird error deep in the guts that I cannot diagnose.
+  If I do a subsequent blacklistAddress with the now-switched-to wallet, is ok.
+  UPDATE: Maybe not... need to examine [object] passed to listeners
+
+  Stuff to be investigated/implemented...
+
+  Web3.givenProvider  // <-- what is this, do we care?
+  web3.eth.accounts.getAccounts()  // <-- do we care?
+  //// To sign a transaction
+  web3.eth.accounts.signTransaction({
+  to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
+  value: '1000000000',
+  gas: 2000000
+  }, '0x4c0883a69102937d6231471b5dbb6204fe5129617082792ae468d01a3f362318')
+  .then(console.log);
+  //// To resolve a *.eth name
+  web3.eth.ens.resolver('ethereum.eth').then(function (contract) {
+  console.log(contract);
+  });
+*/
