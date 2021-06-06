9f82200e57c99339529545a21852891ceb5cb59a
Mike Burr <mburr@unintuitive.org>
2021-06-06 15:41:21 -0400

    somewhat arbitrary checkpoint. lots broken.

--- www/index.js
+++ www/index.js
@@ -12,122 +12,68 @@
 
   * Mike Burr <mb@unintuitive.org>
   * Adam Soper <adam@americanretailusa.com>
-*/
+  */
+
+// This makes "blockies" ... SOMEHOW
+!function(){function e(e){for(var o=0;o<c.length;o++)c[o]=0;for(var o=0;o<e.length;o++)c[o%4]=(c[o%4]<<5)-c[o%4]+e.charCodeAt(o)}function o(){var e=c[0]^c[0]<<11;return c[0]=c[1],c[1]=c[2],c[2]=c[3],c[3]=c[3]^c[3]>>19^e^e>>8,(c[3]>>>0)/(1<<31>>>0)}function r(){var e=Math.floor(360*o()),r=60*o()+40+"%",t=25*(o()+o()+o()+o())+"%",l="hsl("+e+","+r+","+t+")";return l}function t(e){for(var r=e,t=e,l=Math.ceil(r/2),n=r-l,a=[],c=0;t>c;c++){for(var i=[],f=0;l>f;f++)i[f]=Math.floor(2.3*o());var s=i.slice(0,n);s.reverse(),i=i.concat(s);for(var h=0;h<i.length;h++)a.push(i[h])}return a}function l(o){var t={};return t.seed=o.seed||Math.floor(Math.random()*Math.pow(10,16)).toString(16),e(t.seed),t.size=o.size||8,t.scale=o.scale||4,t.color=o.color||r(),t.bgcolor=o.bgcolor||r(),t.spotcolor=o.spotcolor||r(),t}function n(e,o){var r=t(e.size),l=Math.sqrt(r.length);o.width=o.height=e.size*e.scale;var n=o.getContext("2d");n.fillStyle=e.bgcolor,n.fillRect(0,0,o.width,o.height),n.fillStyle=e.color;for(var a=0;a<r.length;a++)if(r[a]){var c=Math.floor(a/l),i=a%l;n.fillStyle=1==r[a]?e.color:e.spotcolor,n.fillRect(i*e.scale,c*e.scale,e.scale,e.scale)}return o}function a(e){var e=l(e||{}),o=document.createElement("canvas");return n(e,o),o}var c=new Array(4),i={create:a,render:n};"undefined"!=typeof module&&(module.exports=i),"undefined"!=typeof window&&(window.blockies=i)}();
+
+var curTab = 0;// 0 = My Address, 1 = Search
+var tabContainer = document.getElementsByClassName("horizontal-tab-container")[0];
+var tabs = document.getElementsByClassName("control-option");
+var searchTabBlock = document.getElementsByClassName("search-tab-block")[0];
 
 if (typeof window.ethereum === 'undefined') {
-  alert('Wallet provider is NOT Available!!');  // How do we respond?
+    alert('Wallet provider is NOT Available!!');
 }
 
-// Notice the return value of isConnected() is not assigned. This may be useful for other things, but
-// I noticed that after calling "isConnected", "window.ethereum.selectedAddress" gets set. In other words
-// It looks like "window.ethereum.selectedAddress" getting assined an address is a side-effect of isConnected()
-// Otherwise, window.ethereum.selectedAddress is null, even when metamask says "connected". If metamask is
-// NOT connected, then window.ethereum.selectedAddress does not get set :-/
-//window.ethereum.isConnected()
-// Damnit! No. This does not seem to have that effect. On loading, the value of window.ethereum.selectedAddress
-// is still a big mystery!
-
-// If window.ethereum.selectedAddress has a non-null value at this point, we could disable the [Enable Ethereum]
-// button here -- $("#enableEthereumButton")
 console.log("selectedAddress we are using upon loading: " + window.ethereum.selectedAddress);
 
 function logEvent(eventName, content, _error) {
-  let message = "[event fired: '" + eventName + "'] " + "content=" + JSON.stringify(content);
-  if (_error) {
-    message = message + ", error=" + JSON.stringify(_error);
-  }
-  console.log(message);
+    let message = "[event fired: '" + eventName + "'] " + "content=" + JSON.stringify(content);
+    if (_error) {
+	message = message + ", error=" + JSON.stringify(_error);
+    }
+    console.log(message);
 }
 
-var web3 = new Web3(window.ethereum);  // FYI, Web3.givenProvider appears to be an alias for window.ethereum
+var web3 = new Web3(window.ethereum);
 
-// Start window.etherem events...
-
-// passes passes connectionInfo which may or may not have a chainId memeber.
 window.ethereum.on('connect', function(connectInfo) {
-  logEvent('connect', connectInfo);
-  //
-  // This event appears to be about "connected to network foobar for the first time" and that's it. When you load the page
-  // for the first time, if MM is enabled, even if you are "not connected", this will fire with the chain that MM is pointing
-  // to.
-  //
-  // WEIRD! -- if we are not "connected" in the MM sense, this event still fires. "we are connected to network foo", even
-  // though MM says "not connected". This 'connect' seems to be ONLY about chains connecting for the first time. Not when
-  // chains switch. If I "connect" in the wallet sense, this does not fire!
-  // this fires when you eth_requestAccounts ... NOT when the pages gets loaded and you're already connected
-  // Update, ok, now I see {"chainId":"0x1"} ... I guess the passed arg is context-dependant. Watch this space.
-  // You might expect switching to a network would fire this. Nope. Look to 'chainChanged' for that.
-  //
-  if (typeof connectInfo.chainId && connectInfo.chainId === "0x4") {
-    // code that responds to "we are now on rinkeby"
-  } else if (typeof connectInfo.chainId && connectInfo.chainId !== "0x4") {
-    // code that responds to "we are now on some chain but it's not rinkeby"
-  } else {
-    debugger;
-    // this message did not have a `chainId` (is this an error?)
-  }
+    logEvent('connect', connectInfo);
+    if (typeof connectInfo.chainId && connectInfo.chainId === "0x4") {
+	// rinkeby
+    } else if (typeof connectInfo.chainId && connectInfo.chainId !== "0x4") {
+	// not rinkeby
+    } else {
+	// we should not get here!
+    }
 });
 
-// passes ..nothing? empty string?
 window.ethereum.on('disconnect', function(content) {
-  // I do not recall seeing this fire. Update here.
-  logEvent('disconnect', content);
+    logEvent('disconnect', content);
 });
 
 // passes account="0xffff...." (string, address)
 window.ethereum.on('accountsChanged', function(account) {
-  logEvent('accountsChanged', account);
-  // It looks like this fires if you press "Enable Ethereum" and end up having to "Confirm".
-  // If you click it and get no confirm, this will not fire, but
-  //
-  //   window.ethereum.request({ method: 'eth_requestAccounts' })
-  //
-  // will return the address(es) regardless.
-  //
-  // An empty array means we are not connected at all.
-  //
-  // Sublty: it looks like the "accounts" returned when not connected is an empty array, but
-  // window.ethereum.selectedAddress gets set to `null`
-  //
-  // If and only if `accounts === []` should we enable $("#enableEthereumButton")
-  // If account has a non-empty value $("#enableEthereumButton") should be DISABLED here
-  // (...I think)
+    logEvent('accountsChanged', account);
 });
 
 // passes toChain="0x5" (A STRING!!!!!!)
 // [see: https://chainid.network/]
 window.ethereum.on('chainChanged', function(toChain) {
-  logEvent('chainChanged', toChain);
-  // NOTE: this DOES fire even when the user is not "connected"
-  // We COULD have separate testbeds for each testnet . The contract address (or even the existence of the contract)
-  // will be chain-dependent.
-  //
-  // Or... we could simply have a mapping:
-  //
-  //  abandonedAddressesContractMapping = {"0x5": "0xffffff..."}  // mapping of chain ID and contract address.
-  //
-  // Change the contract value, re-init ABANDONED_ADDRESSES and put up a conspicous banner if we are on a testnet
-  // (maybe nothing if mainnet)
-  //
-  // Update: It looks like this https://github.com/gnosis/safe-contracts/blob/186a21a74b327f17fc41217a927dea7064f74604/contracts/GnosisSafe.sol#L36 might have the same goal.
-  //
-  // Also, using the same info (contract address) we could have a link to the correct etherscan page for the contract.
-  // Note that when you e.g. load the page 'connect' with chainId is fired. When you witch chains, this is fired. They
-  // are different events.
-  if (toChain === "0x4") {
-    // code that responds to "we switched from chain X to Y, where Y=0x4
-  } else if (toChain !== "0x4") {
-    // code that responds to "we switched from chain X to Y, where Y!=0x4
-  } else {
-    // Handle this! I don't recall seeing anything land here.
-    alert("handle me!");
-  }
+    logEvent('chainChanged', toChain);
+    if (toChain === "0x4") {
+	// switched to rinkeby
+    } else if (toChain !== "0x4") {
+	// switchhed from rinkeby
+    } else {
+	// do we get here?
+    }
 });
 
 // passes ...some message
 window.ethereum.on('message', function(content) {
-  logEvent('message', content);
-  // Anything that shows up here should be explicitly handled
+    logEvent('message', content);
 });
 
 // If we're going to be using globals, they should appear at the top of the file.
@@ -135,58 +81,100 @@ var ABANDONED_ADDRESSES = new web3.eth.Contract(ABI, CONTRACT_ADDRESS); // set b
 
 // We have only one EVM (contract) event to worry about: "AddressAbandoned"
 ABANDONED_ADDRESSES.events.AddressAbandoned({fromBlock: "earliest"}, function(incoming) {
-  // Turns out `incoming` is `null` ...wtf?
-  // Also, this can either take a looong time, or possibly never comes sometimes. Strange!
-  logEvent('abandoned_evm_event', incoming);
+    logEvent('abandoned_evm_event', incoming);
 });
 
 async function getAccount() {
-  window.ethereum.request({ method: 'eth_requestAccounts' }).then((accounts) => {
-    console.log("we succesfully connected to *A* wallet. the data we got was `" + JSON.stringify(accounts) + "`");
-  });
+    window.ethereum.request({ method: 'eth_requestAccounts' }).then((accounts) => {
+	updateThing();
+	console.log("we succesfully connected to *A* wallet. the data we got was `" + JSON.stringify(accounts) + "`");
+    });
 }
 
 $("#enableEthereumButton").click(function() {
-  getAccount();
+    getAccount();
 });
 
 $("#getIsAbandoned").click(function() {
-  let address = $("#isAbandoned").val();
-  transaction = ABANDONED_ADDRESSES.methods.isAbandoned(address);
-  transaction.call({from: window.ethereum.selectedAddress}, function (error, isAbandoned) {
-    if (!error) {
-      if (isAbandoned) {
-        console.log(address + " IS abandoned");
-      } else {
-        console.log(address + " IS NOT abandoned");
-      }
-    } else {
-      console.log("error while checking abandoned status: " + error);
-    }
-  });
+    let address = $("#isAbandoned").val();
+    transaction = ABANDONED_ADDRESSES.methods.isAbandoned(address);
+    transaction.call({from: window.ethereum.selectedAddress}, function (error, isAbandoned) {
+	if (!error) {
+	    if (isAbandoned) {
+		console.log(address + " IS abandoned");
+	    } else {
+		console.log(address + " IS NOT abandoned");
+	    }
+	} else {
+	    console.log("error while checking abandoned status: " + error);
+	}
+    });
 });
 
 $("#abandonAddress").click(function() {
-  transaction = ABANDONED_ADDRESSES.methods.abandonAddress(window.ethereum.selectedAddress);
-  transaction.send({from: window.ethereum.selectedAddress})
-    .on('transactionHash', function(hash){
-      logEvent('transactionHash', hash);
-    })
-    .on('confirmation', function(confirmationNumber, receipt) {
-      // NOTE: we abuse error= field for confirmation number
-      logEvent('confirmation', receipt, "(conf_num=" + confirmationNumber + "[not_an_error])");
-      // We have the confirmation number here. If someone abandons an address, we could have a widget
-      // that shows a confirmation count
-    })
-    .on('receipt', function(receipt){
-      logEvent('receipt', receipt);
-    })
-    .on('error', function(error, receipt) {
-      // If the transaction was rejected by the network with a receipt,
-      // the second parameter will be the receipt.
-      logEvent('error', receipt, error);
-    })
-    .finally(function() {
-      // always runs (for each event?)
-    });
+    transaction = ABANDONED_ADDRESSES.methods.abandonAddress(window.ethereum.selectedAddress);
+    transaction.send({from: window.ethereum.selectedAddress})
+	.on('transactionHash', function(hash){
+	    logEvent('transactionHash', hash);
+	})
+	.on('confirmation', function(confirmationNumber, receipt) {
+	    // NOTE: we abuse error= field for confirmation number
+	    logEvent('confirmation', receipt, "(conf_num=" + confirmationNumber + "[not_an_error])");
+	})
+	.on('receipt', function(receipt){
+	    logEvent('receipt', receipt);
+	})
+	.on('error', function(error, receipt) {
+	    logEvent('error', receipt, error);
+	})
+	.finally(function() {
+	    // finally do this always
+	});
 });
+
+function switchTab(elem) {
+    if(curTab === 0) {
+        curTab = 1;
+        tabContainer.style.marginLeft = "-100%";
+        tabs[0].classList.remove("selected");
+        tabs[1].classList.add("selected");
+    } else {
+        curTab = 0;
+        tabContainer.style.marginLeft = "0";
+        tabs[0].classList.add("selected");
+        tabs[1].classList.remove("selected");
+    }
+}
+
+function checkAddress() {
+    var address = document.getElementById("addressSearch").value;
+    // TODO: Should check for success of address look up
+    var isValidAddress = searchTabBlock.classList.contains("hidden");
+    if(isValidAddress) {
+        searchTabBlock.classList.remove("hidden");
+        searchTabBlock.classList.remove("error");
+    } else {
+        searchTabBlock.classList.add("hidden");
+        searchTabBlock.classList.add("error");
+    }
+}
+
+function getIdenticon(address) {
+    return blockies.create({
+        seed: address,
+        size: 15,
+        scale: 3,
+        spotcolor: '-1'
+    });
+}
+
+function updateThing() {
+    let address = "";
+    if (typeof window.ethereum.selectedAddress !== 'undefined') {
+	address = window.ethereum.selectedAddress;
+    }
+    let icon = getIdenticon(address);
+    let icon2 = getIdenticon(address);
+    document.getElementById("identicon").appendChild(icon);
+    document.getElementById("identiconSearch").appendChild(icon2);
+}
