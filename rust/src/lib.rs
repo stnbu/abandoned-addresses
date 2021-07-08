aa0fe9a1a10407d3f3d433fedf22b6adf00a27aa
Mike Burr <mb@unintuitive.org>
2021-07-08 11:26:39 -0600

    arbitrary stopping point. we have a wasm function that does not just `run` and returns a string value.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,16 +4,16 @@ macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-#[wasm_bindgen(start)]
-pub fn start() -> Result<(), JsValue> {
+#[wasm_bindgen]
+pub fn do_wasm() -> String {
     let window = web_sys::window().expect("could not get a `window`");
     let document = window.document().expect("could not get a `document`");
     let body = document.body().expect("could not get a `body`");
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello abandoned-addresses!");
-    body.append_child(&val)?;
+    let val = document.create_element("p").unwrap();
+    val.set_inner_html("Hello abandoned-addresses2!");
+    body.append_child(&val).unwrap();
     console_log!("Congratulations, WASM works here!");
-    Ok(())
+    "bob".to_string()
 }
 
 #[wasm_bindgen]
