d7ac50b1c378ee09fdd484dba61b9298236f43f3
Mike Burr <mb@unintuitive.org>
2021-07-14 16:05:05 -0600

    untabify; more logs.

--- www/index.etheres.js
+++ www/index.etheres.js
@@ -1,10 +1,10 @@
 // This is the test, "Do we have Metamask or not?"
 if (typeof window.ethereum === 'undefined') {
     alert("Using this site requires that you have a EIP1193-capable environment. " +
-	  "The easy fix: " +
-	  "Installing and setting up Metamask takes about 2 minutes. " +
-	  "Sorry. Install Metamask and reload this page for the full " +
-	  "expirience. See: https://metamask.io/");
+          "The easy fix: " +
+          "Installing and setting up Metamask takes about 2 minutes. " +
+          "Sorry. Install Metamask and reload this page for the full " +
+          "expirience. See: https://metamask.io/");
 } else {
     // if this is "null" we are definitely not connected in any sense.
     console.log("selectedAddress we are using upon loading: " + window.ethereum.selectedAddress);
@@ -18,24 +18,25 @@ window.ethereum.on('accountsChanged', function(account) {
     // to another connected address. The `account` value will never be a non-connected address.
     console.log("Provider switched to account " + account);
     console.log("However... " +
-		"This may not necessarily be the account that shows in the " +
-		"Metamask UI! If someone as two addresses, both connected to " +
-		"this site, and they 'disconnect' the _active_ Metamask address, " +
-		"then the switched-to address above is _not_ the active Metamask " +
-		"address [i.e. the one the user sees the identicon for in the " +
-		"Metamask UI]");
+                "This may not necessarily be the account that shows in the " +
+                "Metamask UI! If someone as two addresses, both connected to " +
+                "this site, and they 'disconnect' the _active_ Metamask address, " +
+                "then the switched-to address above is _not_ the active Metamask " +
+                "address [i.e. the one the user sees the identicon for in the " +
+                "Metamask UI]. And more confusion: Switching accounts in Metamask " +
+		"does not always switch the address here!");
     console.log("Note that `window.ethereum.selectedAddress` now has value " + window.ethereum.selectedAddress);
 });
 
 window.ethereum.on('chainChanged', function(toChain) {
     console.log("Chain changed to " + toChain);
     if (toChain === "0x4") {
-	console.log("Switched to Rinkeby");
+        console.log("Switched to Rinkeby");
     } else {
-	alert("Sorry. Only the Rinkeby test network is supported. " +
-	      "Feel free to file an issue on the abandoned-addresses " +
-	      "Github, or email me at mb@unintuitive.org and I will " +
-	      "happily send you plenty of rETH ");
+        alert("Sorry. Only the Rinkeby test network is supported. " +
+              "Feel free to file an issue on the abandoned-addresses " +
+              "Github, or email me at mb@unintuitive.org and I will " +
+              "happily send you plenty of rETH ");
     }
 });
 
@@ -52,8 +53,9 @@ async function getAccount() {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         signerContract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
-        providerContract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
         console.log("Sucessfuly created global signing contract.");
+        providerContract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+        console.log("Sucessfuly created global provider contract.");
     });
 }
 
